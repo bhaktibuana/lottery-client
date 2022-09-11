07ed9e1b1bc4b16f63dbc94e91d9b4d14d90fefb
bhaktibuana <bhaktibuana19@gmail.com>
2022-09-11 16:39:58 +0700

    test web3 connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { web3 } from "./utils";
 import "./App.scss";
 
 const App = () => {
+  web3.eth.getAccounts().then((result) => {
+    console.log(result);
+  });
+
+  const getData = async () => {
+    await window.ethereum.enable();
+    const account = await web3.eth.getAccounts();
+
+    console.log(account);
+  };
   return (
     <>
       <div>
         <h1>Hello World!</h1>
+
+        <button onClick={getData}>click me</button>
       </div>
     </>
   );
