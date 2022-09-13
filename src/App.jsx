6245cdea69635af0fd6459a329d3f5ba00300d6b
bhaktibuana <bhaktibuana19@gmail.com>
2022-09-13 14:12:05 +0700

    configure routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import React from "react";
-import { web3 } from "./utils";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Home } from "./pages";
+
 import "./App.scss";
 
 const App = () => {
-  web3.eth.getAccounts().then((result) => {
-    console.log(result);
-  });
-
-  const getData = async () => {
-    await window.ethereum.enable();
-    const account = await web3.eth.getAccounts();
-
-    console.log(account);
-  };
   return (
     <>
-      <div>
-        <h1>Hello World!</h1>
-
-        <button onClick={getData}>click me</button>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
