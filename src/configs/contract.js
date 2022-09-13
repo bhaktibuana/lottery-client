e824aa53a3aaebca369e3454c715383b99bb4759
bhaktibuana <bhaktibuana19@gmail.com>
2022-09-13 14:10:14 +0700

    change contract abi and address

--- src/configs/contract.js
+++ src/configs/contract.js
@@ -1,7 +1,7 @@
 import { web3 } from "../utils";
 
 const contract = {
-  address: "0xcf89Bb2CbE07317E52b7BA94A30AC80Be8a07477",
+  address: "0x13Df218B6e55906fcb79c71aBd33f5Afdd0C4Ee3",
   abi: [
     {
       inputs: [],
@@ -28,6 +28,27 @@ const contract = {
       signature: "0x8b5b9ccc",
     },
     {
+      inputs: [{ internalType: "uint256", name: "index", type: "uint256" }],
+      name: "getWinnerInfo",
+      outputs: [
+        { internalType: "uint256", name: "", type: "uint256" },
+        { internalType: "address", name: "", type: "address" },
+      ],
+      stateMutability: "view",
+      type: "function",
+      constant: true,
+      signature: "0x6b1da364",
+    },
+    {
+      inputs: [],
+      name: "getWinnerListLength",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+      constant: true,
+      signature: "0x0831fb6c",
+    },
+    {
       inputs: [],
       name: "manager",
       outputs: [{ internalType: "address", name: "", type: "address" }],
@@ -52,6 +73,14 @@ const contract = {
       type: "function",
       constant: true,
       signature: "0xf71d96cb",
+    },
+    {
+      inputs: [],
+      name: "setWinnerList",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+      signature: "0x21a9b5ab",
     },
   ],
 };
